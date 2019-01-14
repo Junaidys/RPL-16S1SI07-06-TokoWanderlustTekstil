69f8e58ca963f79d1af7e222e225febb1ada6912
Junaidys <43653313+Junaidys@users.noreply.github.com>
2019-01-14 22:10:39 +0700

    Update DataPenjualan.vb

--- UAS/Form/DataPenjualan.vb
+++ UAS/Form/DataPenjualan.vb
@@ -1,3 +1,20 @@
 ﻿Public Class DataPenjualan
+Public Class TampilDataPenjualan
+    Dim baris As Integer
+    Private Sub TampilDataPenjualan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RefreshGrid()
+    End Sub
+
+    Private Sub RefreshGrid()
+        DTGrid = KontrolDataPenjualan.tampilData.ToTable
+        DGDataPenjualan.DataSource = DTGrid
+
+        If DTGrid.Rows.Count > 0 Then
+            baris = DTGrid.Rows.Count - 1
+            DGDataPenjualan.CurrentCell = DGDataPenjualan.Item(1, baris)
+
+        End If
+    End Sub
+End Class
 
 End Class
