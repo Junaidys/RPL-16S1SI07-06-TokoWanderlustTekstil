3ba3d7d2a8b0140ec868b82eeab9e2248cf953e1
Muhammadirfanfebrianto <junaidi.sarifullah@students.amikom.ac.id>
2019-01-14 21:42:51 +0700

    wanderlust tekstil

---
+++ aplikasifix/Global/ModObjekdanFungsi.vb
@@ -0,0 +1,14 @@
+﻿Module ModObjekdanFungsi
+    Public EntitasAdmin As New ClsEntAdmin
+    Public KontrolAdmin As New ClsCtlAdmin
+    Public EntitasKasir As New ClsEntKasir
+    Public KontrolKasir As New ClsCtlKasir
+    Public EntitasKain As New ClsEntKain
+    Public KontrolKain As New ClsCtlKain
+    Public EntitasSupplier As New ClsEntSupplier
+    Public KontrolSupplier As New ClsCtlSupplier
+    Public EntitasPenjualan As New ClsEntPenjualan
+    Public KontrolPenjualan As New ClsCtlPenjualan
+    Public EntitasDetailJual As New ClsEntDetailJual
+
+End Module
